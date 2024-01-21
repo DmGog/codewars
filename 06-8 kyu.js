a0884957f9ae21f1a0e0976e9586703d855ea467
Dmitriy <108808756+DmGog@users.noreply.github.com>
2024-01-21 16:18:19 +0300

    06-8 kyu.js

---
+++ 06-8 kyu.js
@@ -0,0 +1,10 @@
+/* However, in some special cases, the length of the transformed string may be longer than the original. Can you find a string that satisfies this criteria?
+
+For example, in Ruby, That means str.upcase.downcase.size > str.size
+
+You should just set the value of STRANGE_STRING to meet the previous criteria.
+
+Note: Meta programming is not allowed in this kata. So, the size of your solution is limited. */
+
+// Решение ( суррогатные пары)
+const STRANGE_STRING = 'ΰ'
