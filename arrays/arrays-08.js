713220f4353b816756325a53e89ff9ef1c9030eb
Dmitriy <goglev37@gmail.com>
2024-08-26 13:01:18 +0300

    codewars

--- arrays/arrays-08.js
+++ arrays/arrays-08.js
@@ -22,3 +22,45 @@ function multipleOfIndex(array) {
 }
 
 console.log(multipleOfIndex([0, 2, 3, 6, 9]))
+
+
+/*
+If you've completed this kata already and want a bigger challenge, here's the 3D version
+
+Bob is bored during his physics lessons so he's built himself a toy box to help pass the time. The box is special because it has the ability to change gravity.
+
+There are some columns of toy cubes in the box arranged in a line. The i-th column contains a_i cubes. At first, the gravity in the box is pulling the cubes downwards. When Bob switches the gravity, it begins to pull all the cubes to a certain side of the box, d, which can be either 'L' or 'R' (left or right). Below is an example of what a box of cubes might look like before and after switching gravity.
+
++---+                                       +---+
+|   |                                       |   |
++---+                                       +---+
+    +---++---+     +---+              +---++---++---+
+|   ||   |     |   |   -->        |   ||   ||   |
++---++---+     +---+              +---++---++---+
+    +---++---++---++---+         +---++---++---++---+
+|   ||   ||   ||   |         |   ||   ||   ||   |
++---++---++---++---+         +---++---++---++---+
+    Given the initial configuration of the cubes in the box, find out how many cubes are in each of the n columns after Bob switches the gravity.
+
+Examples (input -> output:
+* 'R', [3, 2, 1, 2]      ->  [1, 2, 2, 3]
+* 'L', [1, 4, 5, 3, 5 ]  ->  [5, 5, 4, 3, 1]*/
+
+const flip = (d, a) => {
+    return d === "R" ? a.sort((a, b) => a - b) : a.sort((a, b) => a - b).reverse()
+}
+
+console.log(flip("L", [3, 2, 1, 2]))
+
+/*
+SpeedCode #2 - Array Madness  https://www.codewars.com/kata/56ff6a70e1a63ccdfa0001b1*/
+function arrayMadness(a, b) {
+    // Ready, get set, GO!!!
+    let arrayA = a.map(a => a ** 2).reduce((acc, current) => acc + current)
+    let arrayB = b.map(b => b ** 3).reduce((acc, current) => acc + current)
+
+    return arrayA > arrayB
+
+}
+
+console.log(arrayMadness([4, 5, 6], [1, 2, 3]))
