4084162c4efa62acf81d10ce4247698f74b91dcf
Dmitriy <goglev37@gmail.com>
2024-08-29 09:39:50 +0300

    codewars

--- arrays/arrays-06.js
+++ arrays/arrays-06.js
@@ -7,7 +7,7 @@
 //     [12, 10, 8, 12, 7, 6, 4, 10, 12, 10]          -->  12
 //     [12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]  -->   3
 
-function highestRank(arr){
+function highestRank(arr) {
     const frequencyMap = {};
 
     // Подсчет частоты каждого числа
@@ -33,3 +33,30 @@ function highestRank(arr){
 }
 
 console.log(highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12]))
+
+// Highest Scoring Word https://www.codewars.com/kata/57eb8fcdf670e99d9b000272/train/javascript
+
+function high(x) {
+    function calculateWordScore(word) {
+        let score = 0;
+        for (let i = 0; i < word.length; i++) {
+            score += word.charCodeAt(i) - 96;
+        }
+        return score;
+    }
+    const words = x.split(' ');
+    let highestScore = 0;
+    let highestScoringWord = '';
+
+    for (const word of words) {
+        const score = calculateWordScore(word);
+        if (score > highestScore) {
+            highestScore = score;
+            highestScoringWord = word;
+        }
+    }
+
+    return highestScoringWord;
+}
+
+console.log(high("man i need a taxi up to ubud"))
