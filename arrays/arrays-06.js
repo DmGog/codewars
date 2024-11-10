9a62255d2290e7d094c596503246d032a627e565
Dmitriy <goglev37@gmail.com>
2024-11-10 14:48:29 +0300

    array

--- arrays/arrays-06.js
+++ arrays/arrays-06.js
@@ -196,6 +196,7 @@ console.log(findEvenIndex([20,10,30,10,10,15,35]))*/
 // console.log(reverseArray([])); // []
 // Попробуйте реализовать эту задачу!
 
+/*
 function reverseArray(arr) {
     if (arr.length === 0) {
         return []
@@ -204,4 +205,46 @@ function reverseArray(arr) {
 }
 
 let c = reverseArray([1, 2, 3, 4, 5])
-console.log(c)
+console.log(c)*/
+
+/*
+
+Given an array of integers of any length, return an array that has 1 added to the value represented by the array.
+
+    If the array is invalid (empty, or contains negative integers or integers with more than 1 digit), return nil (or your language's equivalent).
+
+Examples
+Valid arrays
+
+    [4, 3, 2, 5] would return [4, 3, 2, 6] (4325 + 1 = 4326)
+    [1, 2, 3, 9] would return [1, 2, 4, 0] (1239 + 1 = 1240)
+    [9, 9, 9, 9] would return [1, 0, 0, 0, 0] (9999 + 1 = 10000)
+    [0, 1, 3, 7] would return [0, 1, 3, 8] (0137 + 1 = 0138)
+Invalid arrays
+
+    [] is invalid because it is empty
+    [1, -9] is invalid because -9 is not a non-negative integer
+    [1, 2, 33] is invalid because 33 is not a single-digit integer  https://www.codewars.com/kata/5514e5b77e6b2f38e0000ca9/train/javascript*/
+
+
+function upArray(arr) {
+    if (arr.length === 0) {
+        return 0
+    }
+    const minus = arr.some(num => num < 0 || num > 9);
+    if (!minus) {
+        for (let i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] < 9) {
+                arr[i] += 1;
+                return arr;
+            }
+            arr[i] = 0;
+        }
+
+        return [1, ...arr];
+    }
+
+    return 0
+}
+
+console.log(upArray([0, 8]))
--- strings/strings.js
+++ strings/strings.js
@@ -272,3 +272,57 @@ console.log(contamination("_3ebzgh4","&"))*/
     }
 
 console.log(usdcny(15))*/
+
+/*
+The input is a string str of digits. Cut the string into chunks (a chunk here is a substring of the initial string) of size sz (ignore the last chunk if its size is less than sz).
+
+If the sum of a chunk's digits is divisible by 2, reverse that chunk; otherwise rotate it to the left by one position. Put together these modified chunks and return the result as a string.
+
+If
+
+sz is <= 0 or if str == "" return ""
+sz is greater (>) than the length of str it is impossible to take a chunk of size sz hence return "".
+    Examples:
+("123456987654", 6) --> "234561876549"
+("123456987653", 6) --> "234561356789"
+("66443875", 4) --> "44668753"
+("66443875", 8) --> "64438756"
+("664438769", 8) --> "67834466"
+("123456779", 8) --> "23456771"
+("", 8) --> ""
+("123456779", 0) --> ""
+("563000655734469485", 4) --> "0365065073456944"
+Example of a string rotated to the left by one position:
+    s = "123456" gives "234561".*/
+
+// https://www.codewars.com/kata/56b5afb4ed1f6d5fb0000991/train/javascript
+
+/*
+function revrot(str, sz) {
+    if (str.length === 0 || sz <= 0) return "";
+
+    let res = [];
+
+    for (let i = 0; i < str.length; i += sz) {
+        res.push(str.slice(i, i + sz));
+    }
+
+    let result = res.map((e) => {
+        if (e.length < sz) return "";
+
+        const sum = e.split("").reduce((a, b) => Number(a) + Number(b), 0);
+        let arr = e.split("");
+
+        if (sum % 2 === 0) {
+            return arr.reverse().join("");
+        } else {
+            const firstElement = arr.splice(0, 1)[0];
+            arr.push(firstElement);
+            return arr.join("");
+        }
+    });
+
+    return result.join("");
+}
+
+console.log(revrot("123456987654", 6))*/
