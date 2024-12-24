db2b1028375b031d252d75d5f246ff4bf7c5dfb7
Dmitriy <goglev37@gmail.com>
2024-12-24 10:34:27 +0300

    array

--- arrays/arrays-01-06.js
+++ arrays/arrays-01-06.js
@@ -422,12 +422,12 @@ Array.prototype.odd = function() {
 
 
 // Data Reverse https://www.codewars.com/kata/569d488d61b812a0f7000015/train/javascript
-function dataReverse(data) {
-    let res = []
-    for (let i = 0; i < data.length; i = i+8) {
-        res.push(data.slice(i, i+8))
-    }
-    return res.reverse().flat()
-}
-
-console.log(dataReverse([1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]))
+// function dataReverse(data) {
+//     let res = []
+//     for (let i = 0; i < data.length; i = i+8) {
+//         res.push(data.slice(i, i+8))
+//     }
+//     return res.reverse().flat()
+// }
+//
+// console.log(dataReverse([1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]))
